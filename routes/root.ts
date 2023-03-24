882284c0a7a6c39e0327f07daa4c1efda817479d
Aris Ripandi <riipandi@gmail.com>
2023-03-25 05:10:52 +0700

    customize logger

--- routes/root.ts
+++ routes/root.ts
@@ -5,9 +5,9 @@ import { version } from '../server.ts'
 const app = new Hono()
 
 app.get('/', (c: Context) => {
-  // const runtime = `${c.runtime} ${Deno.version.deno}` || 'unknown'
+  const runtimeVersion = Deno.version.deno || 'unknown'
   return jsonResponse(c, `Fastrue ${version}`, {
-    runtime: `${c.runtime}`,
+    runtime: `${c.runtime} v${runtimeVersion}`,
   })
 })
 
