c3768e68c4ec14fd798e3eb9e5094f596c56ba5e
Aris Ripandi <riipandi@gmail.com>
2023-03-25 07:10:50 +0700

    add missing admin handler

--- routes/admin.ts
+++ routes/admin.ts
@@ -11,4 +11,8 @@ app.put('/users/:user_id', (c: Context) => {
   return jsonResponse(c, `admin update user endpoint`)
 })
 
+app.post('/generate_link', (c: Context) => {
+  return jsonResponse(c, `admin generate_link endpoint`)
+})
+
 export { app as adminRoute }
